6e4d017ddf25630818ce14468597ba5b43a8f93b
arun-kumar-ak <arun1812.a@gmail.com>
2020-09-27 10:40:57 +0530

    build folder addeed

---
+++ build/precache-manifest.b9d9a70cf1c23888f6bf7c105d768ed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd60a753173b9050e8ae0b572a014e1",
+    "url": "/heart-disease-detector/index.html"
+  },
+  {
+    "revision": "59ed1083a7b39feeea74",
+    "url": "/heart-disease-detector/static/css/main.e29bde83.chunk.css"
+  },
+  {
+    "revision": "076ac34b8b5f41e674c0",
+    "url": "/heart-disease-detector/static/js/2.5ddc51a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/heart-disease-detector/static/js/2.5ddc51a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ed1083a7b39feeea74",
+    "url": "/heart-disease-detector/static/js/main.665b7501.chunk.js"
+  },
+  {
+    "revision": "49f1dbcfca1a7d625035",
+    "url": "/heart-disease-detector/static/js/runtime-main.7aa18e3b.js"
+  }
+]);
