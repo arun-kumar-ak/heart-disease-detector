c653744eea5f0f25d3564a58256f231c1ca8fcb2
arun-kumar-ak <arun1812.a@gmail.com>
2020-10-01 17:19:56 +0530

    solve input tag bug and add some details to the fieldset or legend

--- src/App.js
+++ src/App.js
@@ -65,10 +65,10 @@ class App extends Component {
         <fieldset>
           <legend><b>Age</b></legend>
           <label>Enter Your Age:</label><br />
-          <input id="age-label" value={this.state.age_value}></input>
+          <input id="age-label" value={this.state.age_value} onChange={(Event)=>{this.setState({age_value:Event.target.value})}}></input>
           <input type="range" min="1" className="slider" max="100" step="1" onChange={(Event)=>{this.setState({age_value:Event.target.value})}}></input><br />
         </fieldset>
-        <fieldset>
+        <fieldset className="equal">
           <legend><b>Sex</b></legend>
           <label htmlFor="male" >Male</label>
           <input  type="radio" name="gender" value={1} onChange={(Event)=>{this.setState({sex_value:Event.target.value})}}></input>
@@ -78,12 +78,12 @@ class App extends Component {
         <fieldset>
           <legend><b>Chest Pain Type</b></legend>
           <label className="font">              
-            0: Typical angina: chest pain related decrease blood supply to the heart<hr /><br />
-            1: Atypical angina: chest pain not related to heart<hr /><br />
-            2: Non-anginal pain: typically esophageal spasms (non heart related)<hr /><br />
+            0: Typical angina: chest pain related decrease blood supply to the heart<br />
+            1: Atypical angina: chest pain not related to heart<br />
+            2: Non-anginal pain: typically esophageal spasms (non heart related)<br />
             3: Asymptomatic: chest pain not showing signs of disease<hr /> <br />
           </label>
-          <input type="number" value={this.state.cp_value}></input>
+          <input type="number" value={this.state.cp_value} onChange={(Event)=>{this.setState({cp_value:Event.target.value})}}></input>
           <input type="range" min="0" max="3" step="1" className="slider" onChange={(Event)=>{this.setState({cp_value:Event.target.value})}}></input>
         </fieldset>
         <fieldset>
@@ -100,7 +100,7 @@ class App extends Component {
           <input type="range" min="94" className="slider" max="200" step="1" onChange={(Event)=>{this.setState({trestbps_value:Event.target.value})}}></input><br />
         </fieldset>
         <fieldset>
-          <legend><b>cholestrol</b></legend>
+          <legend><b>Cholestrol</b></legend>
           <label className="font">
             The person’s cholesterol measurement in mg/dl - above 200 is cause for concern.<hr /><br />
             Minimum : 126<br />
@@ -112,7 +112,7 @@ class App extends Component {
           <input value={this.state.chol_value} onChange={(Event)=>{this.setState({chol_value:Event.target.value})}}></input>
           <input className="slider" type="range" min="126" max="564" step="1" onChange={(Event)=>{this.setState({chol_value:Event.target.value})}}></input><br />
         </fieldset>            
-        <fieldset>
+        <fieldset className="equal">
           <legend><b>Fasting blood sugar</b></legend>            
           <span /><label htmlFor="fbs_no" >No</label>
           <input  type="radio" name="fbs" value={0} onChange={(Event)=>{this.setState({fbs_value:Event.target.value})}}></input>
@@ -120,23 +120,34 @@ class App extends Component {
           <input  type="radio" name="fbs" value={1} onChange={(Event)=>{this.setState({fbs_value:Event.target.value})}}></input><br />
         </fieldset>
         <fieldset>
-          <legend><b>restecg</b></legend>      
+          <legend><b>Restecg</b></legend>      
           <label className="font">
             Resting electrocardiographic results:<hr /><br />
             0 : Normal<br />
             1 : having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV)<br />
             2 : showing probable or definite left ventricular hypertrophy by Estes' criteria<hr />
           </label>      
-          <input  type="number" value={this.state.restecg_value}></input>
+          <input  type="number" value={this.state.restecg_value} onChange={(Event)=>{this.setState({restecg_value:Event.target.value})}}></input>
           <input type="range" className="slider" min="0" max="2" step="1" onChange={(Event)=>{this.setState({restecg_value:Event.target.value})}}></input>
         </fieldset>
         <fieldset>
-          <legend><b>thalach</b></legend>            
+          <legend><b>Thalach</b></legend>  
+          <label>Maximum Heart Rate Achieved.<hr /><br />
+            Minimum : 71<br />
+            25% : 143<br />
+            50% : 153<br />
+            75% : 166<br />
+            Maximum : 202<hr />
+          </label><br />
           <input value={this.state.thalach_value} onChange={(Event)=>{this.setState({thalach_value:Event.target.value})}}></input>
           <input className="slider" type="range" min="71" max="202" step="1" onChange={(Event)=>{this.setState({thalach_value:Event.target.value})}}></input><br />
         </fieldset>      
-        <fieldset>
-          <legend><b>exang</b></legend>          
+        <fieldset className="equal">
+          <legend><b>Exang</b></legend>     
+          <label>
+            Exercise induced Angina, which means heart pain produced during exercise due to lack of oxygen supply.<br />
+            It feel better when relax.
+          </label><hr /><br />
           <span /><label htmlFor="exang_no" >No</label>
           <input  type="radio" name="exang" value={0} onChange={(Event)=>{this.setState({exang_value:Event.target.value})}}></input>
           <label htmlFor="exang_yes">Yes</label>
@@ -144,22 +155,42 @@ class App extends Component {
         </fieldset>
         <fieldset>
           <legend><b>Oldpeak</b></legend>            
-          <input type="number" value={this.state.oldpeak_value}></input>
-          <input className="slider" type="range" min="0" max="6.2" step="0.2" onChange={(Event)=>{this.setState({oldpeak_value:Event.target.value})}}></input><br />
+          <label>
+            ST depression induced by exercise relative to rest looks at stress of heart during excercise unhealthy heart will stress more.<br />            
+          </label><hr /><br />
+          <input type="number" value={this.state.oldpeak_value} onChange={(Event)=>{this.setState({oldpeak_value:Event.target.value})}}></input>
+          <input className="slider" type="range" min="0" max="6.2" step="0.1" onChange={(Event)=>{this.setState({oldpeak_value:Event.target.value})}}></input><br />
         </fieldset>
         <fieldset>
-          <legend><b>slope</b></legend>            
-          <input type="number" value={this.state.slope_value}></input>
+          <legend><b>Slope</b></legend>     
+          <label>
+            The slope of the peak exercise ST segment.<hr /><br />
+            0 : Upsloping: better heart rate with excercise (uncommon)<br />
+            1 : Flatsloping: minimal change (typical healthy heart)<br />            
+            2 : Downslopins: signs of unhealthy heart<br />
+          </label><hr /><br />
+          <input type="number" value={this.state.slope_value} onChange={(Event)=>{this.setState({slope_value:Event.target.value})}}></input>
           <input type="range" className="slider" min="0" max="2" step="1" onChange={(Event)=>{this.setState({slope_value:Event.target.value})}}></input>
         </fieldset>
         <fieldset>
-          <legend><b>ca</b></legend>            
-          <input  type="number" value={this.state.ca_value}></input>
+          <legend><b>CA</b></legend>
+          <label>
+            Number of major vessels (0-3) colored by flourosopy.<hr /><br />
+            colored vessel means the doctor can see the blood passing through.<br />
+            the more blood movement the better (no clots)
+          </label><hr /><br />
+          <input  type="number" value={this.state.ca_value} onChange={(Event)=>{this.setState({ca_value:Event.target.value})}}></input>
           <input type="range" className="slider" min="0" max="4" step="1" onChange={(Event)=>{this.setState({ca_value:Event.target.value})}}></input>
         </fieldset>
         <fieldset>
-          <legend><b>thal</b></legend>            
-          <input  type="number" value={this.state.thal_value}></input>
+          <legend><b>Thal</b></legend>     
+          <label>
+            Thalium stress result.<hr /><br />
+            (1,3) : normal<br />
+            6 : fixed defect: used to be defect but ok now<br />
+            7 : reversable defect: no proper blood movement when excercising.
+          </label><hr /><br />
+          <input  type="number" value={this.state.thal_value} onChange={(Event)=>{this.setState({thal_value:Event.target.value})}}></input>
           <input type="range" className="slider" min="0" max="3" step="1" onChange={(Event)=>{this.setState({thal_value:Event.target.value})}}></input>
         </fieldset>
         <div id="output">
